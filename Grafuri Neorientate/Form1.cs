5570d82a241f2c8b42c7d569bf16044cc5a80773
Șelaru Daniel <danyselaru@gmail.com>
2023-10-09 18:48:14 +0300

    clasa graf

--- Grafuri Neorientate/Form1.cs
+++ Grafuri Neorientate/Form1.cs
@@ -21,10 +21,31 @@ namespace Grafuri_Neorientate
         {
             List<(int a, int b)> muchii = new List<(int a, int b)>();
             muchii.Add((1, 2));
-            muchii.Add((2, 3));
+            muchii.Add((2, 5));
             muchii.Add((1, 3));
-            Graf g = new Graf(3, muchii);
+            muchii.Add((4, 3));
+            muchii.Add((1, 4));
+            Graf g = new Graf(5, muchii);
+            Console.WriteLine("Afisare numar noduri");
             Console.WriteLine(g.NrNoduri);
+
+            Console.WriteLine("Afisare adiacenta nodului 1");
+            Console.WriteLine(g.adiacenta(1));
+
+            Console.WriteLine("Afisare matricea de adiacenta a grafului");
+            Console.WriteLine(g.afisare());
+
+            Graf lanturi = g.matriceaLanturilor(g);
+            Console.WriteLine("Afisare matricea lanturilor");
+            Console.WriteLine(lanturi.afisare());
+
+            Graf h = new Graf(g - 3);
+            Console.WriteLine("Afisare graf fara nodul 3");
+            Console.WriteLine(h.afisare());
+
+            Graf i = new Graf (~g);
+            Console.WriteLine("Afisare graf complementar");
+            Console.WriteLine(i.afisare());
         }
     }
 }
--- Grafuri Neorientate/Graf.cs
+++ Grafuri Neorientate/Graf.cs
@@ -35,7 +35,7 @@ namespace Grafuri_Neorientate
         public Graf()
         {
             nrNoduri = 0;
-            muchii = new int[0,0];
+            muchii = new int[0, 0];
             Initializare();
         }
 
@@ -48,6 +48,7 @@ namespace Grafuri_Neorientate
             _muchii.ForEach(muchie =>
             {
                 muchii[muchie.n1, muchie.n2] = 1;
+                muchii[muchie.n2, muchie.n1] = 1;
             });
         }
 
@@ -55,10 +56,15 @@ namespace Grafuri_Neorientate
         public Graf(Graf G)
         {
             nrNoduri = G.nrNoduri;
+            muchii = new int[nrNoduri + 1, nrNoduri + 1];
+            for (int i = 1; i <= nrNoduri; i++)
+                for (int j = 1; j <= nrNoduri; j++)
+                    muchii[i, j] = G.muchii[i, j];
         }
 
         //d) returnarea numărului de noduri  
-        public int NrNoduri {
+        public int NrNoduri
+        {
             get
             {
                 return nrNoduri;
@@ -70,13 +76,76 @@ namespace Grafuri_Neorientate
         }
 
         //e) afisare lista adiacență pentru un nod dat 
-        //public string adiacenta(int nod)
-        //{
-         
-        //}
+        public string adiacenta(int nod)
+        {
+            string s = "";
+            for (int i = 1; i <= nrNoduri; i++)
+                if (muchii[nod, i] == 1)
+                    s += i.ToString() + " ";
+            return s;
+        }
 
-        //public static Graf oprator -(Graf G, int nod){
-            
-        //}
+        //f) determinare matricea lanțurilor
+        public Graf matriceaLanturilor(Graf G)
+        {
+            Graf aux = new Graf(G);
+            for (int k = 1; k < G.nrNoduri; k++)
+                for (int i = 1; i <= G.nrNoduri; i++)
+                    for (int j = 1; j <= G.nrNoduri; j++)
+                        if (i != j)
+                            if (aux.muchii[i, j] == 0)
+                                aux.muchii[i, j] = aux.muchii[i, k] * aux.muchii[k, j];
+
+            return aux;
+        }
+
+        //g) operatorul – pentru eliminarea unui nod și returnarea subgrafului obținut
+        public static Graf operator -(Graf Gf, int nod)
+        {
+            Graf G = new Graf(Gf);
+            for (int i = 1; i <= G.nrNoduri; i++){
+                G.muchii[i, nod] = 0;
+                G.muchii[nod, i] = 0;
+            }
+
+            //pentru fiecare rand de sub nod ridicam elementele o pozitie
+            for (int i = nod + 1; i <= G.nrNoduri; i++)
+                for (int j = 1; j <= G.nrNoduri; j++)
+                    G.muchii[i - 1, j] = G.muchii[i, j];
+
+            //pentru fiecare coloana din dreapta nodului mutam elementele cu o pozitie in stanga
+            for (int j = nod + 1; j <= G.nrNoduri; j++)
+                for (int i = 1; i <= G.nrNoduri; i++)
+                    G.muchii[i, j - 1] = G.muchii[i, j];
+
+            G.nrNoduri--;
+            return G;
+        }
+
+        //h) operatorul ~ pentru determinare graf complementar
+        public static Graf operator ~(Graf Gf)
+        {
+            Graf G = new Graf(Gf);
+            for (int i = 1; i <= G.nrNoduri; i++)
+                for (int j = 1; j <= G.nrNoduri; j++)
+                    if (G.muchii[i, j] == 0)
+                        G.muchii[i, j] = 1;
+                    else
+                        G.muchii[i, j] = 0;
+            return G;
+        }
+
+        //i) metode  pentru citire / scriere
+        public string afisare()
+        {
+            string s = "";
+            for (int i = 1; i <= nrNoduri; i++)
+            {
+                for (int j = 1; j <= nrNoduri; j++)
+                    s += muchii[i, j].ToString() + " ";
+                s += '\n';
+            }
+            return s;
+        }
     }
 }
